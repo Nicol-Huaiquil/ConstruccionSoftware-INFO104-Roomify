fbf3fed933870a9c12146df07550482e6d9bd498
Andrés Torres <andres.torres01@alumnos.uach.cl>
2020-11-14 18:33:29 -0300

    botones andres

--- src/components/Botones.js
+++ src/components/Botones.js
@@ -3,18 +3,43 @@ import { Button, Flex, Input, Stack, useToast } from "@chakra-ui/core";
 export const Botones = () => {
   return (
     <Stack
+      direction="row"
       shadow="md"
       borderWidth="1px"
       width="fit-content"
-      padding="20px"
+      padding="10px"
       borderRadius="10px"
       alignSelf="center"
     >
       <Flex>
-        <Button bg="#2980b9" width="50px" height="50px" type="submit"></Button>
-        <Button bg="#ad1fe2" width="50px" height="50px" type="submit"></Button>
-        <Button bg="#1fe25f" width="50px" height="50px" type="submit"></Button>
-        <Button bg="#2980b9" width="50px" height="50px" type="submit"></Button>
+        <Button
+          bg="#2980b9"
+          width="50px"
+          height="50px"
+          type="submit"
+          margin="10px"
+        ></Button>
+        <Button
+          bg="#ad1fe2"
+          width="50px"
+          height="50px"
+          type="submit"
+          margin="10px"
+        ></Button>
+        <Button
+          bg="#1fe25f"
+          width="50px"
+          height="50px"
+          type="submit"
+          margin="10px"
+        ></Button>
+        <Button
+          bg="#2980b9"
+          width="50px"
+          height="50px"
+          type="submit"
+          margin="10px"
+        ></Button>
       </Flex>
     </Stack>
   );
