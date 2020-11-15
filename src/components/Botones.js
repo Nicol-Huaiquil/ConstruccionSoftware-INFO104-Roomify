fe0ae562a9d8ac1ad3d814d6773e9d77aa86ffcd
Andrés Torres <andres.torres01@alumnos.uach.cl>
2020-11-14 22:09:45 -0300

    botones con colores e iconos (no centrados)

--- src/components/Botones.js
+++ src/components/Botones.js
@@ -1,4 +1,10 @@
 import { Button, Flex, Input, Stack, useToast } from "@chakra-ui/core";
+import {
+  AiOutlineArrowLeft,
+  AiOutlineArrowRight,
+  AiFillMessage,
+} from "react-icons/ai";
+import { BsFillBookmarkFill } from "react-icons/bs";
 
 export const Botones = () => {
   return (
@@ -13,32 +19,40 @@ export const Botones = () => {
     >
       <Flex>
         <Button
+          colorScheme="blue"
           bg="#2980b9"
           width="50px"
           height="50px"
           type="submit"
           margin="10px"
+          leftIcon={<AiOutlineArrowLeft size="30px" color="white" />}
         ></Button>
         <Button
+          colorScheme="purple"
           bg="#ad1fe2"
           width="50px"
           height="50px"
           type="submit"
           margin="10px"
+          leftIcon={<BsFillBookmarkFill size="30px" color="white" />}
         ></Button>
         <Button
+          colorScheme="green"
           bg="#1fe25f"
           width="50px"
           height="50px"
           type="submit"
           margin="10px"
+          leftIcon={<AiFillMessage size="30px" color="white" />}
         ></Button>
         <Button
+          colorScheme="blue"
           bg="#2980b9"
           width="50px"
           height="50px"
           type="submit"
           margin="10px"
+          leftIcon={<AiOutlineArrowRight size="30px" color="white" />}
         ></Button>
       </Flex>
     </Stack>
