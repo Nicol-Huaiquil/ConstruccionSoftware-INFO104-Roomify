4e1eae544ad5ff1ba748eadf5f2e0f3b1ee85d93
Rodolfo <rodolfoseguel.2@protonmail.com>
2020-11-18 19:45:57 -0300

    Pestañas Home y Ejemplos en index (Home no funciona)

---
+++ src/pages/Roomify.js
@@ -0,0 +1,64 @@
+import { Stack, Tab, TabList, Tabs } from "@chakra-ui/core";
+import { useRouter } from "next/router";
+
+const routes = [
+  {
+    path: "/home",
+    text: "Home",
+  },
+  {
+    path: "/ejemplos",
+    text: "Ejemplos",
+  },
+];
+
+export const Roomify = ({ children }) => {
+  const { push, pathname } = useRouter();
+  return (
+    <Stack>
+      <Tabs
+        index={routes.findIndex((ruta) => ruta.path === pathname)}
+        alignSelf="center"
+      >
+        <TabList>
+          {routes.map((ruta) => {
+            return (
+              <Tab
+                key={ruta.path}
+                onClick={() => {
+                  push(ruta.path);
+                }}
+              >
+                {ruta.text}
+              </Tab>
+            );
+          })}
+        </TabList>
+      </Tabs>
+      {children}
+    </Stack>
+  );
+};
+
+export const RoomifyManual = ({ children }) => {
+  const { push, pathname } = useRouter();
+  return (
+    <Stack>
+      <Tabs
+        index={["/composicion", "/pagina1", "/taller"].indexOf(pathname)}
+        alignSelf="center"
+      >
+        <TabList>
+          <Tab
+            onclick={() => {
+              push("/home");
+            }}
+          >
+            Home
+          </Tab>
+        </TabList>
+      </Tabs>
+      {children}
+    </Stack>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,11 @@
-import { Heading, Stack } from "@chakra-ui/core";
+import { Stack } from "@chakra-ui/core";
 
-import { ProbandoJavascript } from "../components/ProbandoJavascript";
-import { ProbandoAPI } from "../components/ProbandoAPI";
-import { ProbandoListas } from "../components/ProbandoListas";
-import { ProbandoInput } from "../components/ProbandoInput";
-//import { Botones } from "../components/Botones.js";
-//import { Endpoint } from "../components/Endpoint.js";
+import { Roomify } from "../pages/Roomify";
 
 export default function IndexPage() {
   return (
     <Stack spacing="45px" padding="10px">
-      <Heading>Hello World</Heading>
-      <ProbandoJavascript />
-      <ProbandoAPI />
-      <ProbandoListas />
-      <ProbandoInput />
+      <Roomify />
     </Stack>
   );
 }
