e0f12f0d801eb77da0913036884f1bb64f8c4187
Rodolfo <rodolfoseguel.2@protonmail.com>
2020-11-19 18:53:56 -0300

    Se recorren los perfiles de manera circular

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,11 +12,11 @@ import { Roomify } from "../components/Roomify";
 import { useEffect, useState } from "react";
 
 let uId = "14125";
-let i = 0;
 
 export default function Home() {
-  const [apiData, setApiData] = useState();
+  const [profiles, setProfiles] = useState();
   const [loading, setLoading] = useState(true);
+  const [index, setIndex] = useState(0);
   const toast = useToast();
   useEffect(() => {
     axios
@@ -24,110 +24,132 @@ export default function Home() {
         uId: uId,
       })
       .then(({ data }) => {
-        setApiData(data);
+        setProfiles(data);
       })
       .finally(() => {
         setLoading(false);
       });
   }, []);
-  //const dataString = JSON.stringify(data);
-  //setApiData(dataString);
+
+  function increaseIndex() {
+    profiles[index + 1]
+      ? setIndex((index) => index + 1)
+      : setIndex((index) => (index = 0));
+  }
+
+  function decreaseIndex() {
+    profiles[index - 1]
+      ? setIndex((index) => index - 1)
+      : setIndex((index) => (index = 7)); // el 7 debería ser el largo del arreglo profiles
+  }
 
   return (
     <Roomify>
-      {loading && <Spinner />}
-      {apiData ? (
-        <div id="texto">
-          <Text>
-            <div>Nombre: {JSON.stringify(apiData[i].name, null, 2)}</div>
-            <div>Edad: {JSON.stringify(apiData[i].age, null, 2)} años</div>
-            <div>Género: {JSON.stringify(apiData[i].gender, null, 2)}</div>
-            <div>Campus: {JSON.stringify(apiData[i].campus, null, 2)}</div>
-            <div>Carrera: {JSON.stringify(apiData[i].degree, null, 2)}</div>
-            <div>
-              Descripción: {JSON.stringify(apiData[i].description, null, 2)}
+      {loading ? (
+        loading && <Spinner alignSelf="center" />
+      ) : (
+        <div>
+          {profiles ? (
+            <div id="texto">
+              <div>
+                <Text>
+                  Nombre: {JSON.stringify(profiles[index].name, null, 2)}
+                </Text>
+              </div>
+              <div>
+                <Text>
+                  Edad: {JSON.stringify(profiles[index].age, null, 2)} años
+                </Text>
+              </div>
+              <div>
+                <Text>
+                  Género: {JSON.stringify(profiles[index].gender, null, 2)}
+                </Text>
+              </div>
+              <div>
+                <Text>
+                  Campus: {JSON.stringify(profiles[index].campus, null, 2)}
+                </Text>
+              </div>
+              <div>
+                <Text>
+                  Carrera: {JSON.stringify(profiles[index].degree, null, 2)}
+                </Text>
+              </div>
+              <div>
+                <Text>
+                  Descripción:{" "}
+                  {JSON.stringify(profiles[index].description, null, 2)}
+                </Text>
+              </div>
             </div>
-          </Text>
+          ) : null}
+          <div id="bottomButtons">
+            <Stack alignItems="center">
+              <Flex>
+                <Button
+                  bg="#2980b9"
+                  width="50px"
+                  height="50px"
+                  type="submit"
+                  margin="10px"
+                  padding="0px"
+                  onClick={() => {
+                    decreaseIndex();
+                  }}
+                >
+                  <AiOutlineArrowLeft size="30px" color="white" />
+                </Button>
+                <Button
+                  bg="#ad1fe2"
+                  width="50px"
+                  height="50px"
+                  type="submit"
+                  margin="10px"
+                  padding="0px"
+                  onClick={() => {
+                    toast({
+                      title: "Perfil guardado",
+                      duration: 1000,
+                    });
+                  }}
+                >
+                  <BsFillBookmarkFill size="30px" color="white" />
+                </Button>
+                <Button
+                  bg="#1fe25f"
+                  width="50px"
+                  height="50px"
+                  type="submit"
+                  margin="10px"
+                  padding="0px"
+                  onClick={() => {
+                    toast({
+                      title: "Solicitud de mensaje enviada",
+                      duration: 1000,
+                    });
+                  }}
+                >
+                  <AiFillMessage size="30px" color="white" />
+                </Button>
+                <Button
+                  bg="#2980b9"
+                  width="50px"
+                  height="50px"
+                  type="submit"
+                  margin="10px"
+                  padding="0px"
+                  onClick={() => {
+                    increaseIndex();
+                  }}
+                >
+                  <AiOutlineArrowRight size="30px" color="white" />
+                </Button>
+              </Flex>
+            </Stack>
+          </div>
         </div>
-      ) : null}
-      <div id="bottomButtons">
-        <Stack
-          alignItems="center"
-          //direction="row"
-          //shadow="md"
-          //borderWidth="1px"
-          //width="fit-content"
-          //padding="10px"
-          //borderRadius="10px"
-          //alignSelf="center"
-        >
-          <Flex>
-            <Button
-              //colorScheme="blue"
-              bg="#2980b9"
-              width="50px"
-              height="50px"
-              type="submit"
-              margin="10px"
-              padding="0px"
-            >
-              <AiOutlineArrowLeft size="30px" color="white" />
-            </Button>
-            <Button
-              //colorScheme="purple"
-              bg="#ad1fe2"
-              width="50px"
-              height="50px"
-              type="submit"
-              margin="10px"
-              padding="0px"
-              onClick={() => {
-                toast({
-                  title: "Perfil guardado",
-                  duration: 1000,
-                });
-              }}
-            >
-              <BsFillBookmarkFill size="30px" color="white" />
-            </Button>
-            <Button
-              //colorScheme="green"
-              bg="#1fe25f"
-              width="50px"
-              height="50px"
-              type="submit"
-              margin="10px"
-              padding="0px"
-              onClick={() => {
-                toast({
-                  title: "Solicitud de mensaje enviada",
-                  duration: 1000,
-                });
-              }}
-            >
-              <AiFillMessage size="30px" color="white" />
-            </Button>
-            <Button
-              //colorScheme="blue"
-              bg="#2980b9"
-              width="50px"
-              height="50px"
-              type="submit"
-              margin="10px"
-              padding="0px"
-              onClick={() => {
-                i++;
-                toast({
-                  title: "i = " + i,
-                  duration: 1000,
-                });
-              }}
-            >
-              <AiOutlineArrowRight size="30px" color="white" />
-            </Button>
-          </Flex>
-        </Stack>
-      </div>
+      )}
     </Roomify>
   );
 }
